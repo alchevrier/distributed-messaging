6433b2cdbaa6a34fb959dec4bc5166dfd22af35f
Alex Chevrier <chevrier.alex@gmail.com>
2026-02-08 23:11:57 +0800

    feat: Adding graceful shutdown of the broker to avoid write issues

--- broker/src/main/java/io/alchevrier/broker/BrokerApplication.java
+++ broker/src/main/java/io/alchevrier/broker/BrokerApplication.java
@@ -2,10 +2,17 @@ package io.alchevrier.broker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class BrokerApplication {
     static void main(String... args) {
-        SpringApplication.run(BrokerApplication.class);
+        ConfigurableApplicationContext context = SpringApplication.run(BrokerApplication.class);
+
+        // Ensure graceful shutdown on JVM termination (Ctrl+C, kill, etc.)
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("Shutting down broker gracefully...");
+            context.close();
+        }));
     }
 }
